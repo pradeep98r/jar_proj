983bd322ef745e9b2d5b1bd4b01c64a29c302b19
panda <panda@gmail.com>
2023-03-24 19:42:45 +0000

    jar proj is commited

---
+++ src/main/java/panda/cloudbots/www/App.java
@@ -0,0 +1,13 @@
+package panda.cloudbots.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "panda babu gia au" );
+    }
+}
